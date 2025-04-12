325e3a64d037375ec307b4a16e67cc7ab0e4871e
Datodia <dato.diasamidze.02@gmail.com>
2025-04-12 20:35:07 +0400

    fix: post delete only valid user

--- posts/post.service.js
+++ posts/post.service.js
@@ -4,7 +4,7 @@ const userModel = require("../models/user.model")
 
 
 const findAllPosts = async (req, res) => {
-    const posts = await postModel.find().populate('user', 'name email')
+    const posts = await postModel.find().sort({_id: -1}).populate('user', 'name email')
     res.json(posts)
 }
 
@@ -27,13 +27,23 @@ const createPost = async (req, res) => {
 }
 
 const deletePostById = async (req, res) => {
-    const {id} = req.params
-    if(!isValidObjectId(id)) return res.status(400).json({message: 'wrong id is provided'})
+    const { id } = req.params;
+    if (!isValidObjectId(id)) {
+        return res.status(400).json({ message: 'Wrong id is provided' });
+    }
 
-    const deletedPost = await postModel.findByIdAndDelete(id)
-    if(!deletedPost) return res.status(400).json({message: `Cannot delete post by id: ${id}`})
-    
-    await userModel.updateOne({_id: req.userId}, {$pull: {posts: deletedPost._id}})
+    const post = await postModel.findById(id);
+
+    if (post.user.toString() !== req.userId) {
+        return res.status(403).json({ message: "You are not authorized to delete this post" });
+    }
+
+    const deletedPost = await postModel.findByIdAndDelete(id);
+
+    await userModel.updateOne(
+        { _id: req.userId },
+        { $pull: { posts: deletedPost._id } }
+    );
 
     res.json({message: "post deleted successfully", data: deletedPost})
 }
@@ -42,6 +52,11 @@ const updatePostById = async (req, res) => {
     const {id} = req.params
     if(!isValidObjectId(id)) return res.status(400).json({message: 'wrong id is provided'})
     
+    const post = await postModel.findById(id);
+
+    if (post.user.toString() !== req.userId) {
+        return res.status(403).json({ message: "You are not authorized to delete this post" });
+    }
     const updateRequest = {}
     const {content, title} = req.body
     if(content) updateRequest.content = content
