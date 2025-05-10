8bc6aaafc7bcbfc33f1fbbf53a2027a4ab08847c
gikraz <130279660+gikraz@users.noreply.github.com>
2025-05-10 15:58:52 +0400

    hh

--- auth/auth.route.js
+++ auth/auth.route.js
@@ -1,13 +1,3 @@
-const { Router } = require("express");
-const userModel = require("../models/user.model");
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const authRouter = Router()
-const dotenv = require('dotenv');
-const isAuth = require("../middlewares/isAuth");
-dotenv.config()
-
-
 authRouter.post('/sign-up', async (req, res) => {
     const {email, password, name} = req.body
     if(!email || !password || !name) return res.status(400).json({message: "email, password and name is required"})
@@ -18,31 +8,3 @@ authRouter.post('/sign-up', async (req, res) => {
     const hashedPass = await bcrypt.hash(password, 10)
     await userModel.create({email, name, password: hashedPass})
     
-    res.status(201).json({message: "user created successfully"})
-})
-
-
-authRouter.post('/sign-in', async (req, res) => {
-    const {email, password} = req.body
-    if(!email || !password) return res.status(400).json({message: "email and password is required"})
-    const existUser = await userModel.findOne({email})
-    if(!existUser) return res.status(400).json({message: "email or password is invalid"})
-
-    const hassPassedEqual = await bcrypt.compare(password, existUser.password)
-    if(!hassPassedEqual) return res.status(400).json({message: "email or password is invalid"})
-    
-    const payload = {
-        id: existUser._id
-    }
-
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '1h'})
-    res.json(token)
-})
-
-authRouter.get('/current', isAuth, async (req, res) => {
-    const user = await userModel.findById(req.userId)
-    res.json(user)
-})
-
-
-module.exports = authRouter
